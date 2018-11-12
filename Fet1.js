05206ddbacf39a4959ac6e53f678cadd84947cb0
IraKutepova <38941132+IraKutepova@users.noreply.github.com>
2018-11-12 20:51:09 +0700

    Add files via upload

---
+++ Fet1.js
@@ -0,0 +1,174 @@
+
+import { Text, Timers, View, StyleSheet } from 'react-native';
+import { Constants,SecureStore } from 'expo';
+import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
+ import {Base64} from 'js-base64';
+import { Image, AppRegistry,AsyncStorage, TouchableOpacity,Alert, ActivityIndicator, Platform} from 'react-native';
+ import Zapros from './Zapros';
+ import Hr from 'react-native-hr-component'
+export default class FetchUserData extends Component {
+  
+  constructor(props)
+  {
+ 
+    super(props);
+ 
+    this.state = { 
+    isLoading: true,
+	     isLoading2: false,
+
+ dataArray:[],
+ start1: true,
+start:true,
+
+
+  }}
+ 
+  
+  
+  componentDidMount() {
+          const { params } = this.props.navigation.state;
+        
+      return fetch('http://185.185.70.210:8081/api/detail', { 	  
+   method: 'GET', 
+   headers:  {
+     'Authorization': 'Basic'+' '+Base64.btoa(params.username+':'+params.password),
+     'Content-Type': 'application/json',
+   }
+ })
+         .then( (response) =>
+         {
+    if (response.status !== 200) {
+      return (Promise.reject(new Error(response.statusText)),
+      this.setState({
+             isLoading2: true,
+             isLoading: false,
+
+            start1:response.status 
+           } )
+      )
+    }
+
+    return Promise.resolve(response)
+  })
+         .then((response) => response.json())
+         .then((responseJson) => {
+           this.setState({
+             isLoading: false,
+		    isLoading2: false,
+
+             dataSource: responseJson,
+             
+            start1:false,
+           }, function() {
+           
+           });
+         })
+         .catch((error) => {
+           console.error(error);
+         });
+     
+      }
+
+  render() {
+     const { navigate } = this.props.navigation; 
+    
+       const { params } = this.props.navigation.state;
+    let pic1 = {
+      uri: 'https://cdn4.iconfinder.com/data/icons/green-shopper/1049/settings.png'
+    };
+    
+  const {dataSource}= this.state; 
+ function _setUrl(userId,teamId,managerId){
+ const b='http://185.185.70.210:8081/api/timetracking/timesheets?managerId=1441355&period=WEEK&teamId=857&userId=8019';	 
+return  b;
+}
+ if (this.state.isLoading2) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>
+                 Upss... Error:  {this.state.start1}
+                    </Text>
+                    <Text style={styles.paragraph}>
+                 {params.username}
+                    </Text>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        onPress={() => {
+                          navigate('Home')}}>   
+                          <Text style={styles.buttons}>
+                Go to Login
+                    </Text>
+                        
+                        </TouchableOpacity> 
+        </View>
+      );
+ }
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+     
+
+ 
+    return (
+ 
+<View style={styles.container}>
+  <View  style={styles.row1}>
+                    <Text style={styles.paragraph}>
+                   {dataSource.fullName}
+                    </Text>
+                        <TouchableOpacity 
+                        style={styles.button}
+                        onPress={() => {
+                          navigate('Home')}}>   
+                          
+                        <Image source={pic1} style={styles.images1}  />
+                        </TouchableOpacity>
+        </View>
+                   <Text style={styles.textRegister}> ___________________ </Text>   
+                        
+<Zapros url1={_setUrl(dataSource.id,dataSource["assignment"].team.id,dataSource["assignment"].manager.id)} username={params.username} password={params.password}/>
+
+</View>
+            
+    );
+  }
+}
+ 
+const styles = StyleSheet.create({
+
+container: {
+    flex: 1,
+   alignItems: 'center',
+     paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#000080',
+  },
+ row1: { flexDirection: 'row',  height: 50,paddingTop: 10,
+
+         },
+    paragraph: {
+	         margin: 5,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#00FF00',
+  },
+   images1: {
+     width: 50, 
+     height:50,
+    //  margin: 20,
+     },
+       textRegister:{
+     
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#00FF00',
+      },
+  buttons:{textAlign: 'center',fontSize: 30,color: '#000080',  height: 40,width: 200,   backgroundColor: '#00FF00',borderRadius:7,margin: 15,},
+});
+ 
---
+++ LoginScreen.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image,AsyncStorage, TouchableHighlight,TouchableOpacity, TextInput} from 'react-native';
+
+import {Alert, Text, View, StyleSheet} from 'react-native';
+import { Constants, SecureStore} from 'expo';
+
+import App from './App.js';
+import FetchUserData from './Fet1.js';
+import { StackNavigator } from 'react-navigation';
+export default class LoginScreen extends React.Component {
+   constructor(props) {
+    super(props)
+    this.state = {start: true}
+    
+  }
+
+  onPress = () => {
+    this.setState({
+         isLogin: false,
+         hasData: null
+    })
+    
+  }
+setValueLocally=(key,value)=>{
+ 
+    SecureStore.setItemAsync(key, value);
+ 
+ 
+  }
+ 
+  getValueLocally_name=(key)=>{
+ 
+    SecureStore.getItemAsync(key).then((value) => this.setState({ getName : value }))
+ 
+  }
+ 
+  
+   getValueLocally_password=(key)=>{
+ 
+  SecureStore.getItemAsync(key).then((value) => this.setState({ getPass : value }))
+    
+ 
+  }
+ getValueLocally_has=(key)=>{
+ 
+   SecureStore.getItemAsync(key).then((value) => this.setState({ hasData : value }))
+    
+ 
+  }
+  _navigate=(username,password)=>{
+const { navigate } = this.props.navigation;
+{
+  (username==='') ? alert('These fields are required!'):
+  
+  navigate('WorkHours', {username: username, password:password, loginStatus: true});
+}
+  }
+
+  render() {
+ 
+  const { navigate } = this.props.navigation;
+  if (this.state.start)
+{this.getValueLocally_name('name');
+
+ this.getValueLocally_password('password');
+ this.setState({ start : false });
+}
+const readName= this.state.getName;
+
+       return (
+      <View  style={styles.container}>
+             <Text style={styles.paragraph}>Please, login!</Text>
+
+             
+             <Text style={styles.formText}>Name
+        </Text>
+
+    <TextInput
+        style={styles.TextInputStyle}
+        onChangeText={(text) => this.setState({name:text})}
+        value={this.state.name}
+       defaultValue= {this.state.getName} 
+underlineColorAndroid='transparent'
+        editable = {true}
+        maxLength = {40}
+      >
+        
+      </TextInput>
+        <Text style={styles.formText}>
+        
+        Password
+        </Text>
+        
+     <TextInput
+        style={styles.TextInputStyle}
+        secureTextEntry={true}
+        onChangeText={(text1) => this.setState({pass: text1})}
+        
+underlineColorAndroid='transparent'
+        value={this.state.pass}
+        defaultValue= {this.state.getPass} 
+        editable = {true}
+        password = {true}
+        maxLength = {40}
+      >
+      </TextInput>
+ <TouchableHighlight
+         style={styles.button} 
+
+         onPress={() => {
+          
+
+  this.setValueLocally('name', this.state.name);  this.setValueLocally('has', 'true'); this.setValueLocally('password', this.state.pass);
+  this.getValueLocally_name('name');
+ this.getValueLocally_password('password');
+  this._navigate(this.state.getName,this.state.getPass,true)
+
+  }
+  }
+  >
+        
+              <Text style={styles.buttons} >
+                Login
+          </Text>
+       </TouchableHighlight>
+     
+    </View>
+      
+    );
+     }
+  
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  alignItems: 'center',
+     paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#000080',
+  },
+  
+  
+formText:{
+  fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: '#00FF00',margin: 5,
+},
+ paragraph: {
+    margin: 5,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: '#00FF00',
+  },
+   
+  buttons:{ textAlign: 'center',fontSize: 30,color: '#000080',  height: 40,width: 200,   backgroundColor: '#00FF00',borderRadius:7,margin: 15,},
+ 
+TextInputStyle:{
+  color: '#00FF00',
+  
+fontWeight: 'bold',
+  fontSize: 20,
+  textAlign: 'center',
+  margin: 5,
+  height: 40,
+  width: '95%',
+  borderWidth: 2, 
+  borderColor: '#fff',
+  borderRadius: 10
+},
+});
+
